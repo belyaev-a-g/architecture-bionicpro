e5a5d1d673047278ef5a9b021cf33299da9dcb2d
Belyaev Alex <al.g.belyaev@gmail.com>
2026-06-20 16:00:11 +0300

    Добавил заполнение данных OLTP таблицы с телеметрией

--- task1/architecture-DWH-pipeline/test.sh
+++ task1/architecture-DWH-pipeline/test.sh
@@ -10,3 +10,12 @@ docker compose up -d crm_db
 # Работа с данными CRM.
 select c.full_name, p.device_id, p.model_name from prostheses p JOIN customers c ON p.customer_id = c.customer_id where c.login = 'john.doe';
 
+
+
+
+# Работа с БД телеметрии
+docker exec -it bionicpro-telemetry-postgres bash
+docker compose up -d telemetry_db
+docker compose down telemetry_db
+docker compose logs telemetry_db
+
